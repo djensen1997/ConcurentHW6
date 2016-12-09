1f17455a4b65c5381c949b5f24a0a6fda7b8235e
DaneJensen <firstlegoboy@comcast.net>
2016-12-09 16:49:29 -0500

    stuff was done

--- thread.cpp
+++ thread.cpp
@@ -22,7 +22,7 @@
 // FUNCTION CALLED :                                           
 // 		sprintf, srand
 // ----------------------------------------------------------- 
-Index::Index(int* output, int Row, int Col, int uid, 
+Index::Index(int* output, int uid, int Row, int Col, 
 	SynOneToOneChannel* colChan, SynOneToOneChannel* colChanUp, 
 	SynOneToOneChannel* rowChan, SynOneToOneChannel* rowChanLeft)
 :c(output), row(Row), col(Col), up(colChanUp), left(rowChanLeft){
